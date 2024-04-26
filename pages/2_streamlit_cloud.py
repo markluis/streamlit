1f961704b4489a03acef39a4d2f6a0b494a26e7b
markluis <markgabrielluis@gmail.com>
2024-04-26 10:32:04 +0800

    Add files via upload
    updated logic of the session. included the main logic of each page in the if clause

--- pages/2_streamlit_cloud.py
+++ pages/2_streamlit_cloud.py
@@ -3,7 +3,7 @@ import pandas as pd
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 
-if st.session_state.get("logged_in", True):
+if st.session_state.get("logged_in", False):
     if st.button("Log Out", type="secondary"):
         st.session_state.logged_in = False
         st.switch_page("./login.py")
@@ -15,17 +15,17 @@ st.set_option('deprecation.showPyplotGlobalUse', False)
 #region [ CSV Data Sources ]
 
 def load_lyrics_fearless_data():
-    lyrics = pd.read_csv('./data/swift_fearless_taylors_version.csv')["lyric"]
+    lyrics = pd.read_csv('.\data\swift_fearless_taylors_version.csv')["lyric"]
     lyrics_series = ', '.join(str(i) for i in lyrics)
     return lyrics_series
 
 def load_lyrics_1989_data():
-    lyrics = pd.read_csv('./data/swift_1989_deluxe.csv')["lyric"]
+    lyrics = pd.read_csv('.\data\swift_1989_deluxe.csv')["lyric"]
     lyrics_series = ', '.join(str(i) for i in lyrics)
     return lyrics_series
 
 def load_lyrics_folklore_data():
-    lyrics = pd.read_csv('./data/swift_folklore.csv')["lyric"]
+    lyrics = pd.read_csv('.\data\swift_folklore.csv')["lyric"]
     lyrics_series = ', '.join(str(i) for i in lyrics)
     return lyrics_series
 
@@ -34,7 +34,7 @@ def load_all_lyrics():
 
 #endregion
 
-selectbox_album = st.selectbox("Taylor Swift's Album", ['Fearless', '1989', 'Folklore'], index=None, placeholder="Select an album")
+selectbox_album = st.selectbox("Taylor Swift's Album", ['Fearless', '1989', 'Folklore'], index=2, placeholder="Select an album")
 
 if selectbox_album == None:
     # select all albums
@@ -47,7 +47,6 @@ if selectbox_album == None:
     st.pyplot()
 else:
     # load selected album
-    # st.write("Album: ", selectbox_album)
     sb_str = "Most common word in the album: " + selectbox_album
     st.info(sb_str)
     if selectbox_album == 'Fearless':
